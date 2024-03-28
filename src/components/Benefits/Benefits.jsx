4720cd515e8cc645d74294ca991c73abe288628a
rakib <rakib135836@gmail.com>
2024-03-28 21:58:10 +0600

    fixing error

--- src/components/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits.jsx
@@ -9,7 +9,7 @@ const Benefits = () => {
                 <li>Improves Vocabulary: Regular reading enhances vocabulary and language skills, leading to better communication and writing abilities.</li>
                 <li>Reduces Stress: Engaging in a good book can provide a temporary escape from daily stressors, promoting relaxation and mental well-being.</li>
                 <li>Enhances Critical Thinking: Analyzing plotlines, characters, and themes fosters critical thinking skills, enabling you to evaluate information more effectively.</li>
-                <li>Boosts Empathy: Immersing yourself in characters' experiences fosters empathy and emotional intelligence, improving relationships and social interactions.</li>
+                <li>Boosts Empathy: Immersing yourself in characters experiences fosters empathy and emotional intelligence, improving relationships and social interactions.</li>
                 <li>Stimulates Creativity: Exposure to diverse narratives stimulates imagination and creativity, inspiring innovative thinking and problem-solving.</li>
                 <li>Encourages Lifelong Learning: Cultivating a habit of reading encourages a lifelong pursuit of knowledge and personal growth.</li>
             </ul>
